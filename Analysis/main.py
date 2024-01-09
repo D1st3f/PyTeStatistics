b7b654859a1f6f4a12fcc599137eeea055515e56
D1st3f <dima.purig@gmail.com>
2024-01-09 16:27:17 +0200

    Fixed annotations

--- Analysis/main.py
+++ Analysis/main.py
@@ -3,6 +3,7 @@ import os
 from datetime import datetime
 
 import pandas as pd
+from pandas import DataFrame
 
 from Scrapper.manage import get_all_vacancy
 
@@ -10,7 +11,7 @@ logging.basicConfig(format='%(levelname)s - %(message)s',
                     level=logging.INFO)
 
 
-def read_from_csv():
+def read_from_csv() -> DataFrame:
     file_name = f'{datetime.now().strftime("Data %Y-%m-%d")}.csv'
     current_directory = os.path.dirname(os.path.abspath(__file__))
     full_path = os.path.join(current_directory,
@@ -19,8 +20,7 @@ def read_from_csv():
                              file_name)
     if os.path.isfile(full_path):
         try:
-            df = pd.read_csv(full_path)
-            print(df.head())
+            return pd.read_csv(full_path)
         except pd.errors.EmptyDataError:
             logging.error("File is empty!")
             get_all_vacancy()
